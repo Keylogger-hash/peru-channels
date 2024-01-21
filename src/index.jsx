d0f0caa652b0b327284ee7a027fb86a4736acae5
Keylogger-hash <example@example.com>
2024-01-21 21:07:12 +0500

    Fix names

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ export default function App(){
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" basename="/peru-channels">
+                <Route path="/" >
                     <Route index element={<MainDesktop/>}></Route>
                     <Route path="*" element={<Element></Element>}></Route>
                     <Route path="/detail" element={<FilmPage></FilmPage>}></Route>
