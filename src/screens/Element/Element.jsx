bd337c7c3afa0d709a9a93e933b3ddef8d043ac7
Keylogger-hash <example@example.com>
2024-01-22 09:45:11 +0500

    Fix error pageg

--- src/screens/Element/Element.jsx
+++ src/screens/Element/Element.jsx
@@ -1,49 +1,26 @@
 import React from "react";
 import "./style.css";
 import { useNavigate } from "react-router-dom";
-
+import { Header } from "../../components/Header/Header";
+import { Footer } from "../../components/Footer/Footer";
 export const Element = () => {
   let navigate = useNavigate(); 
   const routeChange = () =>{ 
-    let path = `/`; 
+    let path = `/peru-channels/`; 
     navigate(path);
   }
   return (
     <div className="element">
       <div className="frame-wrapper">
         <div className="frame">
-          <header className="header">
-            <div className="text-wrapper">Pelota Libre TV</div>
-            <img className="solar-football-bold" alt="Solar football bold" src="./public/solar-football-bold-duotone.svg" />
-          </header>
+          <Header></Header>
           <div className="hero-page">
             <img className="rectangle" alt="Rectangle" src="./public/rectangle-7.png" />
             <div className="div-wrapper">
               <p className="div" onClick={routeChange}>Take this button to return main page</p>
             </div>
           </div>
-          <footer className="footer">
-            <div className="text-wrapper-2">Social</div>
-            <div className="frame-2">
-              <div className="frame-3">
-                <img className="img" alt="Ri twitter fill" src="./public/ri-twitter-fill.svg" />
-                <div className="text-wrapper-3">Twitter</div>
-              </div>
-              <div className="frame-3">
-                <img className="img" alt="Ri instagram fill" src="./public/ri-instagram-fill.svg" />
-                <div className="text-wrapper-3">Instagram</div>
-              </div>
-              <div className="frame-4">
-                <img className="vector" alt="Vector" src="./public/vector.svg" />
-                <div className="text-wrapper-3">YouTube</div>
-              </div>
-            </div>
-            <div className="frame-5">
-              <div className="text-wrapper-4">Pelota Libre</div>
-              <div className="text-wrapper-5">DMCA</div>
-            </div>
-            <p className="p">Pelota Libre TV 2023 © All rights reserved.</p>
-          </footer>
+          <Footer></Footer>
         </div>
       </div>
     </div>
